efc4587a2fe7eb07359765c4bb690cc18e99782d
Benjamin Wacha <bmwachajr>
2019-09-12 18:08:27 +0300

    design header page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,23 +1,28 @@
 import React from 'react';
 import logo from '../../assets/images/logo.svg';
+import heroImage from '../../assets/images/hero-image.svg';
 import './Header.css';
 
 function Header() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This is the header Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="Header">
+      <header className="Header_content">
+        <div className="Header_text">
+          <h1>Screen recording should be easy</h1>
+          <p>Portail is a beautiful, flexible screen recording app built with the user in mind. This is our public apha shoot some feeedback our way</p>
+
+          <div className="Header_form">
+            <form action="">
+              <input type="text" name="country" value="> brew install portail@alpha" readonly/>
+              <button className="button"> Copy to clipboard </button>
+            </form>
+          </div>
+        </div>
+
+        <div className="Header_hero_image">
+          <img src={heroImage} className="App_logo_img" alt="logo" />
+        </div>
+
       </header>
     </div>
   );
--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,9 +1,15 @@
 import React from '../../../node_modules/react';
 import Header from '../Header';
+import logo from '../../assets/images/logo.svg'
 import './App.css';
 
 const App = props => (
   <div className="container">
+    <div className="App_logo">
+      <img src={logo} className="App_logo_img" alt="logo" />
+      <h1 className="App_logo_text">Portail</h1>
+    </div>
+    
     <Header />
 
   </div>
