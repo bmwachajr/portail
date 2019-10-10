d3dc809c14b9f418c939341ed82bd7a1749dd593
Benjamin Wacha <bmwachajr>
2019-10-10 10:30:31 +0300

    add subscribe component

---
+++ src/components/Subscribe/Subscribe.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import subscribeDash from '../../assets/images/subscribe-dash.svg';
+import './Subscribe.css';
+
+function Subscribe() {
+  return (
+    <div className="Subscribe">
+      <div className="Subscribe_content">
+        <div className="Subscribe-content-dash">
+          <img src={subscribeDash} className="subscribe_dash" alt="dash" />
+        </div>
+
+        <div className="Subscribe_text">
+          <h1>Subscribe to Portail Newsletter</h1>
+          <p>No spam, ever. Your email address will only ever be user for Portail News and you can easily unsubscribe with a single click any time.</p>
+        </div>
+
+        <div className="Subscribe_form">
+          <form action="">
+            <input type="text" name="country" placeholder="your email address" readonly/>
+            <button className="button"> SUBSCRIBE </button>
+          </form>
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default Subscribe;
